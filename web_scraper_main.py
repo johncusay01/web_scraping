fafe96d6a99bc4cea427b4825e2b06e58e4dc741
John Alfred Cusay <John Alfred Cusay@192.168.1.3>
2017-09-16 03:33:43 +0800

    updated

--- web_scraper_main.py
+++ web_scraper_main.py
@@ -2,12 +2,15 @@ from datetime import datetime
 from threading import Timer
 from web_scraper_func import Scraper
 from selenium import webdriver
+import requests
+from bs4 import BeautifulSoup
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait # available since 2.4.0
 from selenium.webdriver.support import expected_conditions as EC # available since 2.26.0
 
 import time
+from xlwt.ExcelFormulaLexer import true_pattern
 
 
 # query the website and return the html to the variable ?page?
@@ -20,52 +23,40 @@ class WebScraper():
     def __init__(self, url):
 
         self.scraper = Scraper(url)
+        self._cont = True
 
-    def body(self):
+    def getLastPage(self, url):
 
-        # self.scraper.get_all()
-        self.scraper.createPage()
-        self.scraper.createContainer()
+        page = requests.get(url)
+        soup = BeautifulSoup(page.text, 'html.parser')
+        
+        _page = soup.find('ul', attrs={
+        'class': 'pagination'},
+                                   limit=None)
+        
+        _page_list = _page.find_all
+        
+        pass
 
-        page = 1
-        # while self.scraper.self.loop_flag:
-        # while page < 2:
-        #
-        #     print 'Page: ' + str(page)
-        #     self.scraper.get_all()
-        #
-        #     page += 1
-        #     self.scraper.url = 'https://fmovies.is/movies?page=' + str(page)
-        #     # self.scraper.url = 'http://www.lazada.com.ph/shop-power-bank/?itemperpage=120&page='+ str(page) + '&sc=EeIb&searchredirect=Power+bank%3Fspm%3Da2o4l.home.0.0.0V7DGc'
-        #     self.scraper.createPage()
-        #     self.scraper.createContainer()
+    def getData(self, movie_list, driver):
 
-        chrome = webdriver.Chrome('C:\Python27\Lib\site-packages\selenium\webdriver\chrome\chromedriver.exe')
-        chrome.get(self.scraper.url)
-        # driver.set_window_position(0, 0)
-        # driver.set_window_size(100000, 200000)
-        # driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        # time.sleep(5)  # wait to load
-        #
-
-        movie_list = chrome.find_element_by_class_name('movie-list')
-
-        # for x in movie_list.find_elements_by_class_name('col-lg-3'):
-        #     print x.get_attribute('innerHTML')
 
         hover_at = movie_list.find_elements_by_class_name('col-lg-3')
+
+        if len(hover_at) == 0:
+            self._cont = False
 
         for i in range(0, len(hover_at)):
 
             print '{' + str(i) + ')'
-
-            hover = ActionChains(chrome).move_to_element(hover_at[i])
+            print hover_at[i].get_attribute('innerHTML')
+            
+            hover = ActionChains(driver).move_to_element(hover_at[i])
             hover.perform()
             time.sleep(1)
 
-            print hover_at[i].get_attribute('innerHTML')
 
-            cluetip = hover_at[i].find_element_by_id('cluetip-1')
+            cluetip = driver.find_element_by_id('cluetip-1')
             cluetip_o = cluetip.find_element_by_class_name('cluetip-outer')
             cluetip_i = cluetip_o.find_element_by_class_name('cluetip-inner')
 
@@ -98,14 +89,49 @@ class WebScraper():
             print 'Genre: ' + genre
             print 'Stars: ' + stars
 
-
+            movie_list = driver.find_element_by_class_name('widget-title')
+            hover_ = ActionChains(driver).move_to_element(movie_list)
+            hover_.perform()
             time.sleep(1)
 
+    def body(self):
+
+        self.scraper.createPage()
+        self.scraper.createContainer()
+
+        
+
+        #=======================================================================
+        # chrome = webdriver.Chrome('C:\Python27\selenium\webdriver\chrome\chromedriver')
+        # chrome.get(self.scraper.url)
+        #=======================================================================
+
+
+
+        page = 490
+        while self._cont:
+            
+            url = self.scraper.url + str(page)
+            chrome = webdriver.Chrome('C:\Python27\selenium\webdriver\chrome\chromedriver')
+            chrome.get(url)
+            chrome.set_window_size(1000, 1000)
+            
+            #===================================================================
+            # try:
+            #     movie_list = chrome.find_element_by_class_name('movie-list')
+            # except:
+            #     break            
+            #===================================================================
+            print '{' + str(page) + '}'
+            movie_list = chrome.find_element_by_class_name('movie-list')
+            self.getData(movie_list, chrome)
+            chrome.quit()
+            page += 1
 
 if __name__ == '__main__':
 
     # url = 'http://www.lazada.com.ph/shop-power-bank/?itemperpage=120&page=1&sc=EeIb&searchredirect=Power+bank%3Fspm%3Da2o4l.home.0.0.0V7DGc'
-    url = 'https://fmovies.is/movies'
+    url = 'https://fmovies.is/movies?page='
 
     web_scraper = WebScraper(url)
     web_scraper.body()
